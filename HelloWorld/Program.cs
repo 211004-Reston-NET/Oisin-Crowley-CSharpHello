1fd6bc0fd8987e52c61d29751aed6e93402b887c
ocrowley1233 <oisin.p.crowley@gmail.com>
2021-10-06 07:52:08 -0400

    Hello World Project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HouseFuction;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            House Oisin = new House();
+
+            Oisin.MiceName = "Stewart";
+
+            Oisin.Owner = "Oisin";
+
+            Console.WriteLine(Oisin.MiceName);
+
+            Console.WriteLine(Oisin.Owner);
+
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("you are " + age + " years old");
+        }
+    }
+}
